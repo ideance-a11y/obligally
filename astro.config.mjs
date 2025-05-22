aa54a7c726f4df9ad40713013c68403de7b1c8ed
olivierstl <olivier.stl@proton.me>
2025-05-22 18:03:53 +0200

    feat: ajout d'i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+// I18n : https://docs.astro.build/fr/guides/internationalization/
+export default defineConfig({
+  i18n: {
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+    routing: {
+      prefixDefaultLocale: true // La locale par défaut est présente dans la route également
+    }
+  }
+})
---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+  en: 'English',
+  fr: 'Français'
+}
+
+export const defaultLang = 'fr'
+
+export const ui = {
+  fr: {
+    'lang.version': 'Version Française',
+    'nav.label': 'Menu principal',
+    'nav.home': 'Accueil',
+    'nav.about': 'À propos'
+  },
+  en: {
+    'lang.version': 'English version',
+    'nav.home': 'Home',
+    'nav.about': 'About'
+  }
+} as const
