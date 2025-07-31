1acf1a95113b202a3b38782cb0bc8e494e69635e
Olivier Stl <olivier.stl@proton.me>
2025-07-31 10:00:00 +0200

    feat: ajout d'un composant disclosure

---
+++ src/components/WrapperDisclosure.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    type="button"
+    :aria-expanded="isDisplayed"
+    v-on:click="isDisplayed = !isDisplayed"
+  >
+    <slot name="button" />
+  </button>
+  <div v-show="isDisplayed">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isDisplayed = ref(false)
+
+/** Liste des slots */
+defineSlots<{
+  button(): unknown
+  default(): unknown
+}>()
+</script>
